0ff7246f12fa8b95916cd745f27763aacdf9853e
Chen <chpyer@gmail.com>
2023-07-16 20:25:11 +0800

    增加钉钉通知; 判断当前音响设备;

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,5 @@
+cd "$(dirname "$0")"
 ps -ef | grep mystake | grep -v "grep" | awk '{print $2}' | xargs kill -9
 unset http_proxy https_proxy all_proxy
-nohup mystake monitor up -f "/Users/Chen/Desktop/Table.txt" -u 3 > /dev/null 2>&1 &
+nohup mystake monitor up -f "/Users/Chen/Desktop/Table.txt" -u 3 -i > /dev/null 2>&1 &
 ps -ef |grep mystake
